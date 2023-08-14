8e348942a6e2eb75950b0c40c8793e24d744dcca
조윤서 <124692313+chobbong@users.noreply.github.com>
2023-08-14 19:51:29 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(
+     page_title="Streamlit App",
+     layout="wide" # layout="centered"
+)
+
+st.header ("부지런한 거위팀")
+st.subheader("부지런한 거위팀의 첫번째 스트림릿 앱입니다.")
+st.write("111111")
---
+++ pages/1_page1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(
+     page_title="Streamlit App",
+     layout="wide" # layout="centered"
+)
+
+st.write("1page")
